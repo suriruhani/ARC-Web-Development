27e747c44ecff5a0fd7fe6734e416f75c6f31472
weizhang05 <chanweizhang05@hotmail.com>
2019-04-13 02:00:34 +0800

    added database dependencies and updated index route with examples

--- App/routes/index.js
+++ App/routes/index.js
@@ -1,8 +1,61 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let pool = require('../pool');
+let router = express.Router();
+
+const firstPlaceQuery = 
+`
+SELECT * FROM score ORDER BY DESC LIMIT 1
+`
+
+const getPlayerScoreQuery = 
+`
+SELECT * FROM score WHERE name = $1
+`
+
+const getAllPlayerNames =
+`
+SELECT name FROM score
+`
+
+/*
+DO NOT CALL THIS Function
+
+Copy paste the reference code instead
+*/
+function sampleCodes(){
+	/*
+	This is an example of a callback function.
+	
+	Executes the SQL query to the database and returns it's results from the database itself.
+	*/
+	pool.query(firstPlaceQuery, (err, data) => {
+		// TODO: console.log(data) to figure how to intepret the data
+		// Hint: It's pretty intuitive. Database has tables. Data stored in tables are in forms of records which are ROWS.
+	});
+	
+	/*
+	This is another example of executing a SQL query but with parameters.
+	*/
+	pool.query(getPlayerScoreQuery, ["parameter for the SQL query here"], (err, data) => {
+	});
+	
+	/*
+	Another way to render and send data over to view side.
+	
+	(Extra) In context, we call this the middleware.
+	*/
+	res.render('Name of View file to be rendered', 
+	{ title: 'Title of the webpage. Equivalent to <title>ABC</title>' }, 
+	key: "value", anotherKey: "Yet another value :3");
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
